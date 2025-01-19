550b67121077df2eccdc76039ee4ee3cef8f15aa
Zoey Goetzinger <zoeyag154252@gmail.com>
2025-01-18 23:55:20 -0600

    base: update register classes to ignore byte order

--- source/base/register.hpp
+++ source/base/register.hpp
@@ -6,107 +6,245 @@
 class Register_16bit
 {
 public:
-	inline uint16_t operator()() const { return (uint16_t&)this->_data[0]; }
-	inline void operator()(uint16_t value) { (uint16_t&)this->_data[0] = std::move(value); }
+	uint16_t operator()() const { return this->_data; }
+	void operator()(const uint16_t value) { this->_data = value; }
 
-	inline uint8_t highByte() const { return this->_data[1]; }
-	inline uint8_t lowByte() const { return this->_data[0]; }
+	uint8_t highByte() const { return static_cast<uint8_t>(this->_data >> 8); }
+	uint8_t lowByte() const { return static_cast<uint8_t>(this->_data & 0xff); }
 
-	inline void highByte(uint8_t value) { this->_data[1] = std::move(value); }
-	inline void lowByte(uint8_t value) { this->_data[0] = std::move(value); }
+	void highByte(const uint8_t value) { this->_data = (this->_data & 0x00ff) | (value << 8); }
+	void lowByte(const uint8_t value) { this->_data = (this->_data & 0xff00) | value; }
 
-	inline void operator++() { ((uint16_t&)this->_data[0])++; }
-	inline void operator--() { ((uint16_t&)this->_data[0])--; }
+	// prefix increment
+	Register_16bit& operator++()
+	{
+		this->_data++;
+		return *this;
+	}
+
+	// postfix increment
+	Register_16bit operator++(int)
+	{
+		Register_16bit old = *this;
+		this->_data++;
+		return old;
+	}
+
+	// prefix decrement
+	Register_16bit& operator--()
+	{
+		this->_data--;
+		return *this;
+	}
+
+	// postfix decrement
+	Register_16bit operator--(int)
+	{
+		Register_16bit old = *this;
+		this->_data--;
+		return old;
+	}
 
 private:
-	std::array<uint8_t, 2> _data {0x00, 0x00};
+	uint16_t _data {0x0000};
 };
 
 class Register_15bit
 {
 public:
-	inline uint16_t operator()() const { return (uint16_t&)this->_data[0]; }
-	inline void operator()(const uint16_t value) { (uint16_t&)this->_data[0] = value & 0x7fff; }
+	uint16_t operator()() const { return this->_data; }
+	void operator()(const uint16_t value) { this->_data = (value & 0x7fff); }
 
-	inline uint8_t highByte() const { return this->_data[1]; }
-	inline uint8_t lowByte() const { return this->_data[0]; }
+	uint8_t highByte() const { return static_cast<uint8_t>(this->_data >> 8); }
+	uint8_t lowByte() const { return static_cast<uint8_t>(this->_data & 0xff); }
 
-	inline void highByte(const uint8_t value) { this->_data[1] = value & 0x7f; }
-	inline void lowByte(uint8_t value) { this->_data[0] = std::move(value); }
-
-	inline void operator++() { (uint16_t&)this->_data[0] = ((*this)() + 1) % 0x8000; }
-	inline void operator--()
+	void highByte(const uint8_t value)
 	{
-		if ((*this)() == 0)
-			(uint16_t&)this->_data[0] = 0x7fff;
+		this->_data = (this->_data & 0x00ff) | ((value & 0x7f) << 8);
+	}
+	void lowByte(const uint8_t value) { this->_data = (this->_data & 0xff00) | value; }
+
+	// prefix increment
+	Register_15bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x8000;
+		return *this;
+	}
+
+	// postfix increment
+	Register_15bit operator++(int)
+	{
+		Register_15bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_15bit& operator--()
+	{
+		if (this->_data == 0)
+			this->_data = 0x7fff;
 		else
-			((uint16_t&)this->_data[0])--;
+			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_15bit operator--(int)
+	{
+		Register_15bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
-	std::array<uint8_t, 2> _data {0x00, 0x00};
+	uint16_t _data {0x0000};
 };
 
 class Register_14bit
 {
 public:
-	inline uint16_t operator()() const { return (uint16_t&)this->_data[0]; }
-	inline void operator()(const uint16_t value) { (uint16_t&)this->_data[0] = value & 0x3fff; }
+	uint16_t operator()() const { return this->_data; }
+	void operator()(const uint16_t value) { this->_data = (value & 0x3fff); }
 
-	inline uint8_t highByte() const { return this->_data[1]; }
-	inline uint8_t lowByte() const { return this->_data[0]; }
+	uint8_t highByte() const { return static_cast<uint8_t>(this->_data >> 8); }
+	uint8_t lowByte() const { return static_cast<uint8_t>(this->_data & 0xff); }
 
-	inline void highByte(const uint8_t value) { this->_data[1] = value & 0x3f; }
-	inline void lowByte(uint8_t value) { this->_data[0] = std::move(value); }
-
-	inline void operator++() { (uint16_t&)this->_data[0] = ((*this)() + 1) % 0x4000; }
-	inline void operator--()
+	void highByte(const uint8_t value)
 	{
-		if ((*this)() == 0)
-			(uint16_t&)this->_data[0] = 0x3fff;
+		this->_data = (this->_data & 0x00ff) | ((value & 0x3f) << 8);
+	}
+	void lowByte(const uint8_t value) { this->_data = (this->_data & 0xff00) | value; }
+
+	// prefix increment
+	Register_14bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x4000;
+		return *this;
+	}
+
+	// postfix increment
+	Register_14bit operator++(int)
+	{
+		Register_14bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_14bit& operator--()
+	{
+		if (this->_data == 0)
+			this->_data = 0x3fff;
 		else
-			((uint16_t&)this->_data[0])--;
+			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_14bit operator--(int)
+	{
+		Register_14bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
-	std::array<uint8_t, 2> _data {0x00, 0x00};
+	uint16_t _data {0x0000};
 };
 
 class Register_10bit
 {
 public:
-	inline uint16_t operator()() const { return (uint16_t&)this->_data[0]; }
-	inline void operator()(const uint16_t value) { (uint16_t&)this->_data[0] = value & 0x03ff; }
+	uint16_t operator()() const { return this->_data; }
+	void operator()(const uint16_t value) { this->_data = (value & 0x03ff); }
 
-	inline uint8_t highByte() const { return this->_data[1]; }
-	inline uint8_t lowByte() const { return this->_data[0]; }
+	uint8_t highByte() const { return static_cast<uint8_t>(this->_data >> 8); }
+	uint8_t lowByte() const { return static_cast<uint8_t>(this->_data & 0xff); }
 
-	inline void highByte(const uint8_t value) { this->_data[1] = value & 0x03; }
-	inline void lowByte(uint8_t value) { this->_data[0] = std::move(value); }
-
-	inline void operator++() { (uint16_t&)this->_data[0] = ((*this)() + 1) % 0x0400; }
-	inline void operator--()
+	void highByte(const uint8_t value)
 	{
-		if ((*this)() == 0)
-			(uint16_t&)this->_data[0] = 0x03ff;
+		this->_data = (this->_data & 0x00ff) | ((value & 0x03) << 8);
+	}
+	void lowByte(const uint8_t value) { this->_data = (this->_data & 0xff00) | value; }
+
+	// prefix increment
+	Register_10bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x0400;
+		return *this;
+	}
+
+	// postfix increment
+	Register_10bit operator++(int)
+	{
+		Register_10bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_10bit& operator--()
+	{
+		if (this->_data == 0)
+			this->_data = 0x03ff;
 		else
-			((uint16_t&)this->_data[0])--;
+			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_10bit operator--(int)
+	{
+		Register_10bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
-	std::array<uint8_t, 2> _data {0x00, 0x00};
+	uint16_t _data {0x0000};
 };
 
 class Register_8bit
 {
 public:
 	inline uint8_t operator()() const { return this->_data; }
-	inline void operator()(uint8_t value) { this->_data = std::move(value); }
+	inline void operator()(const uint8_t value) { this->_data = value; }
 
-	inline void operator++() { this->_data++; }
-	inline void operator--() { this->_data--; }
+	// prefix increment
+	Register_8bit& operator++()
+	{
+		this->_data++;
+		return *this;
+	}
 
-private:
+	// postfix increment
+	Register_8bit operator++(int)
+	{
+		Register_8bit old = *this;
+		this->_data++;
+		return old;
+	}
+
+	// prefix decrement
+	Register_8bit& operator--()
+	{
+		this->_data--;
+		return *this;
+	}
+
+	// postfix decrement
+	Register_8bit operator--(int)
+	{
+		Register_8bit old = *this;
+		this->_data--;
+		return old;
+	}
+
+protected:
 	uint8_t _data {0x00};
 };
 
@@ -116,13 +254,38 @@ public:
 	inline uint8_t operator()() const { return this->_data; }
 	inline void operator()(const uint8_t value) { this->_data = value & 0x3f; }
 
-	inline void operator++() { this->_data = (this->_data + 1) % 0x40; }
-	inline void operator--()
+	// prefix increment
+	Register_6bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x40;
+		return *this;
+	}
+
+	// postfix increment
+	Register_6bit operator++(int)
+	{
+		Register_6bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_6bit& operator--()
 	{
 		if (this->_data == 0)
 			this->_data = 0x3f;
 		else
 			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_6bit operator--(int)
+	{
+		Register_6bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
@@ -135,13 +298,38 @@ public:
 	inline uint8_t operator()() const { return this->_data; }
 	inline void operator()(const uint8_t value) { this->_data = value & 0x1f; }
 
-	inline void operator++() { this->_data = (this->_data + 1) % 0x20; }
-	inline void operator--()
+	// prefix increment
+	Register_5bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x20;
+		return *this;
+	}
+
+	// postfix increment
+	Register_5bit operator++(int)
+	{
+		Register_5bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_5bit& operator--()
 	{
 		if (this->_data == 0)
 			this->_data = 0x1f;
 		else
 			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_5bit operator--(int)
+	{
+		Register_5bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
@@ -154,13 +342,38 @@ public:
 	inline uint8_t operator()() const { return this->_data; }
 	inline void operator()(const uint8_t value) { this->_data = value & 0x07; }
 
-	inline void operator++() { this->_data = (this->_data + 1) % 0x08; }
-	inline void operator--()
+	// prefix increment
+	Register_3bit& operator++()
+	{
+		this->_data = (this->_data + 1) % 0x08;
+		return *this;
+	}
+
+	// postfix increment
+	Register_3bit operator++(int)
+	{
+		Register_3bit old = *this;
+		this->operator++();
+		return old;
+	}
+
+	// prefix decrement
+	Register_3bit& operator--()
 	{
 		if (this->_data == 0)
 			this->_data = 0x07;
 		else
 			this->_data--;
+
+		return *this;
+	}
+
+	// postfix decrement
+	Register_3bit operator--(int)
+	{
+		Register_3bit old = *this;
+		this->operator--();
+		return old;
 	}
 
 private:
